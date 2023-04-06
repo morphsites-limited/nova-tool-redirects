a28d6bffd1364791ecec5986fcb33bd6148b50f2
Chris Ware <chris-ware@users.noreply.github.com>
2023-04-06 16:49:00 +0100

    Update ShouldRedirectMissingPages.php

--- src-php/Traits/ShouldRedirectMissingPages.php
+++ src-php/Traits/ShouldRedirectMissingPages.php
@@ -8,9 +8,10 @@ trait ShouldRedirectMissingPages
      * Retrieve the model for a bound value.
      *
      * @param  mixed  $value
+     * @param  string|null  $field
      * @return \Illuminate\Database\Eloquent\Model|null
      */
-    public function resolveRouteBinding($value)
+    public function resolveRouteBinding($value, $field = null)
     {
         if ($resolved = $this->where($this->getRouteKeyName(), $value)->first()) {
             return $resolved;
